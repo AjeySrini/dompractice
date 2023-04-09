b8be64818b37756f334aeae0c314492d2c82dc40
AjeySrini <ajeysrini@gmail.com>
2023-04-09 17:45:49 +0530

    class practice

---
+++ script.js
@@ -0,0 +1,14 @@
+function foo(){
+    var res=document.getElementById("fname").value;
+    console.log(res);
+    var res1=document.getElementById("mname").value;
+    console.log(res1);
+    var res2=document.getElementById("lname").value;
+    console.log(res2);
+    var res3=document.getElementById("mail").value;
+    console.log(res3);
+    var res4=document.getElementById("pnum").value;
+    console.log(res4);
+   
+}
+
